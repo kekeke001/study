bf7cfaf9f69cbb3e67116d2cb16ceaa3298919e7
kekeke001 <52167708+kekeke001@users.noreply.github.com>
2020-07-07 08:28:16 +0800

    Update linklist_exe.c

--- linklist_exe.c
+++ linklist_exe.c
@@ -118,6 +118,86 @@ LinkList Reverse(LinkList L){//法2
 	return L;
 }
 
+6、有一个带头节点的单链表L，设计一个算法使其元素递增有序
+bool sort(LinkList &L){
+	if(L->next==NULL)
+		return false;
+	LNode *p=L->next;
+	LNode *r=p->next;
+	LNode *pre;
+	p->next=NULL;
+	p=r;
+	while(p!=NULL){
+		r=p->next;
+		pre=L;
+		while(pre->next!=NULL && pre->next->data<p->data)
+			pre=pre->next;
+		p->next=pre->next;
+		pre->next=p;
+		p=r;	
+	}
+}
+
+7、设在一个带表头节点的单链表中所有元素节点的数据值无序，试编写一个函数，删除表中所有介于给定的两个值（作为函数参数给出）之间的元素的元素（若存在）
+bool DelMN(LinkList &L,int min,int max){
+	if(L->next==NULL)
+		return false;
+	if(min > max)
+		return false;
+	LNode *pre=L;
+	LNode *p=pre->next;
+	while(p!=NULL)
+		if(p->data>min && p->data<max)
+			pre->next=p->next;
+			free(p);
+			p=pre->next;
+		else
+			pre=pre->next;
+			p=pre->next;
+	return true;
+}
+
+8、给定两个链表，编写算法找出两个链表的公共节点
+
+9、给定一个带头节点的单链表，设head为头指针，节点结构为（data，next），data为整型元素，next为指针，试写出算法：按递增次序输出单链表中个节点的数据元素，并释放节点所占的存储空间（要求：不允许使用数组作为辅助空间）
+
+10、将一个带头节点的单链表A分解为两个带头节点的单链表A和B，使得A表中含有原表中序号为奇数的元素，而B表中含有原表中序号为偶数的元素，且保持其相对顺序不变
+LinkList Divide(LinkList &LA){
+	if(LA->next==NULL)
+		return NULL;
+	int j=0;
+	LNode *r1=LA;
+	LNode *p=r1->next;
+	r1->next=NULL;
+	LB=(LinkList)malloc(sizeof(LNode));
+	LNode *r2=LB;
+	r2->next=NULL;
+	LNode *q;
+	while(p!=NULL)
+		j++;
+		if(j%2==0)
+			r2->next=p;
+			r2=p;
+		else
+			r1->next=p;
+			r1=p;
+		p=p->next;
+	r1->next=NULL;
+	r2->next=NULL;
+	return LB;
+	
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
 
